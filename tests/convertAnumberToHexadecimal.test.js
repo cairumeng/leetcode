08b416e31effe81cc665ec30976b07f41b8c9e2b
cairumeng <cairumeng@gmail.com>
2021-05-19 10:58:13 +0200

    1805

--- tests/convertAnumberToHexadecimal.test.js
+++ tests/convertAnumberToHexadecimal.test.js
@@ -1,11 +1,11 @@
 import { expect } from '@jest/globals'
 import toHex from '../src/convertAnumberToHexadecimal'
 
-// test('num=26', () => {
-//   const nums = 26
-//   const output = '1a'
-//   expect(toHex(nums)).toEqual(output)
-// })
+test('num=26', () => {
+  const nums = 26
+  const output = '1a'
+  expect(toHex(nums)).toEqual(output)
+})
 test('num=-1', () => {
   const nums = -1
   const output = 'ffffffff'
